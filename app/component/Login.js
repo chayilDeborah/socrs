972773d4cea222e918869779a9c7366327d769a4
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-14 03:28:34 +0100

    feat: create login page

---
+++ app/component/Login.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex justify-center'>
+        <div className='w-full'>
+            <div className='flex'>
+                <div className='h-screen w-[720px] flex justify-center items-center bg-[url("https://res.cloudinary.com/dmye53wps/image/upload/v1702516792/loginbg_rarakk.svg")] '>
+                    <div className='text-[32px] font-bold text-[#fff] my-auto w-[503px] '>Student Online Course Registration System [SOCRS]</div>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <div className='rounded-[16px] border-[1px] border-[#CECECE] pt-[47px] px-[29px] '>
+                        <div>
+                            <div className='text-[16px] font-semibold leading-[22.4px] text-[#00B3D1] mt-[20px] '>Kindly Read the Registration Procedure</div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Login
