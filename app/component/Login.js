825e45845b09c24a1aa72ee5e53dedb657746575
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-14 04:08:57 +0100

    feat: login tab

--- app/component/Login.js
+++ app/component/Login.js
@@ -12,6 +12,10 @@ const Login = () => {
                     <div className='rounded-[16px] border-[1px] border-[#CECECE] pt-[47px] px-[29px] '>
                         <div>
                             <div className='text-[16px] font-semibold leading-[22.4px] text-[#00B3D1] mt-[20px] '>Kindly Read the Registration Procedure</div>
+                            <div className='flex bg-[#fff] w-[386px] rounded-t-[8px] mb-[18px]  '>
+                                <div className='text-[#344054] text-[#fff] text-[16px] w-[193px] leading-[22.4px] font-bold mt-[9px] mb-[10px] text-center ' id='student' >Student</div>
+                                <div className='text-[#344054] text-[#fff] text-[16px] w-[193px] leading-[22.4px] font-bold mt-[9px] mb-[10px] text-center '>Admin</div>
+                            </div>
                         </div>
 
                     </div>
