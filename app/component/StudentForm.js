c091b16b054272b412240c8ff6aaaceb4bf847b0
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-15 02:50:17 +0100

    feat: create studentForm component

---
+++ app/component/StudentForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const StudentForm = () => {
+  return (
+    <>
+    <div className='text-[50px] mt-[50px]'>
+        student is here
+    </div>
+      
+    </>
+  )
+}
+
+export default StudentForm
