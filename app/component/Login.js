21d66b93bd624c1d81f870f28c1b554c63d45515
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-15 03:43:15 +0100

    feat: complete studentForm

--- app/component/Login.js
+++ app/component/Login.js
@@ -28,11 +28,11 @@ const Login = () => {
                     <div className='h-screen w-[720px] flex justify-center items-center bg-[url("https://res.cloudinary.com/dmye53wps/image/upload/v1702516792/loginbg_rarakk.svg")] '>
                         <div className='text-[32px] font-bold text-[#fff] my-auto w-[503px] '>Student Online Course Registration System [SOCRS]</div>
                     </div>
-                    <div className='flex justify-center items-center'>
-                        <div className='rounded-[16px] border-[1px] border-[#CECECE] pt-[47px] px-[29px] '>
+                    <div className='flex justify-center items-center ml-[140px] '>
+                        <div className='rounded-[16px] border-[1px] border-[#CECECE] pt-[47px] px-[29px] w-[443px] '>
                             <div>
-                                <div className='text-[16px] font-semibold leading-[22.4px] text-[#00B3D1] mt-[20px] '>Kindly Read the Registration Procedure</div>
-                                <div className='flex bg-[#fff] w-[386px] rounded-t-[8px] mb-[18px]  '>
+                                <div className='text-[16px] font-semibold leading-[22.4px] text-[#00B3D1] mb-[20px]  '>Kindly Read the Registration Procedure</div>
+                                <div className='flex bg-[#fff] w-[386px] rounded-t-[8px] mb-[26px]  '>
                                     <div className='text-[#344054] text-[16px] w-[193px] rounded-t-[8px] leading-[22.4px] font-bold pt-[9px] pb-[10px] text-center ' onClick={() => handleTabClick('student')} style={{ backgroundColor: activeTab === 'student' ? '#00B3D1' : 'transparent', color: '#F2F4F7' }} >Student</div>
                                     <div className='text-[#344054] text-[#000] text-[16px] rounded-t-[8px] w-[193px] leading-[22.4px] font-bold pt-[9px] pb-[10px] text-center ' onClick={() => handleTabClick('admin')} style={{ backgroundColor: activeTab === 'admin' ? '#00B3D1' : 'transparent', color: '#F2F4F7' }} >Admin</div>
                                 </div>
