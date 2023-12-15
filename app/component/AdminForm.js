437ae35d25237fc64d93c858cfe09c97846b939c
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-15 02:53:43 +0100

    feat: create AdminForm component

---
+++ app/component/AdminForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AdminForm = () => {
+  return (
+    <>
+    <div className='text-[50px] mt-[50px]'>
+        admin is here
+    </div>
+      
+    </>
+  )
+}
+
+export default AdminForm
