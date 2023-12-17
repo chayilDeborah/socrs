2b51389976f8a1773e0d0e1d933f560dd0f99053
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-17 19:33:28 +0100

    feat: create HOD page

---
+++ app/hod/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import HodDashboard from '../component/HodDashboard'
+
+const HOD = () => {
+  return (
+    <>
+    <div className='flex justify-center bg-[#F2F4F7]'>
+      <div className='lg:max-w-[1440px] w-full '>
+        <div>
+            <HodDashboard />
+        </div>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default  HOD
