103e0397fb9701755704f556339fa387068a77ac
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-17 19:34:03 +0100

    feat: create Hod dashboard

---
+++ app/component/HodDashboard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import HodNav from './HodNav'
+
+const HodDashboard = () => {
+  return (
+    <>
+    <div className='flex justify-center'>
+        <div className='w-full'> 
+        <div className='flex'>
+            <HodNav />
+
+        </div>
+
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default HodDashboard
