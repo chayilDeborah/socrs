a52203d0c82cf8752f7c9dc9ee9bee052f41c50e
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-20 19:50:29 +0100

    feat: create dean page and nav

---
+++ app/dean/page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import DeanDashboard from '../component/DeanDashboard'
+
+const Dean = () => {
+  return (
+    <>
+    <div className='flex justify-center bg-[#F2F4F7]'>
+      <div className='lg:max-w-[1440px] w-full '>
+        <div>
+            <DeanDashboard />
+        </div>
+      </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Dean
