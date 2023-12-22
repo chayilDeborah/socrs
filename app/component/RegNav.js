ad02202c50c19c33f9aef9a2273f08fc337795f5
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-12-22 03:57:38 +0100

    feat: create and complete reg nav

--- app/component/RegNav.js
+++ app/component/RegNav.js
@@ -1,6 +1,6 @@
+import Image from 'next/image'
 import React from 'react';
-import Image from 'next/image';
-import donald from '../assets/donald.png'
+import admin from '../assets/admin.png'
 import Link from 'next/link';
 import settings from '../assets/settings.svg'
 import students from '../assets/students.svg'
@@ -9,46 +9,50 @@ import dash from '../assets/dash.svg';
 import course from '../assets/course.svg';
 import notification from '../assets/notification.svg';
 import help from '../assets/help.svg';
+import usertick from '../assets/usertick.svg'
 import logout from '../assets/logout.svg'
 
-const AdminNav = () => {
+const RegNav = () => {
   return (
     <>
-      <div className='flex justify-center bg-[#fff] border-[1px] border-[#CECECE] pt-[64px] pl-[70px] pr-[34px] '>
+    <div className='flex justify-center bg-[#fff] border-[1px] border-[#CECECE] pt-[64px] pl-[70px] pr-[34px] '>
         <div>
-          <Image src={donald} alt='donald' className='mx-auto bg-[#D9D9D9] p-[11px] rounded-[50%] ' />
-          <div className='text-[24px] font-bold text-[#344054] mt-[10px] w-[224px] text-center '>Donald Ressler</div>
-          <div className='text-[#344054] opacity-[0.5] text-[16px] font-bold text-center '>Admin</div>
-          <div className='my-[64px] text-[#344054] '>
-            <div className='flex hover:text-[#00B3D1]'>
+        <Image src={admin} alt='admin' className='mx-auto' />
+          <div className='text-[24px] font-bold text-[#344054] mt-[10px] w-[224px] text-center '>Tom Keen </div>
+            <div className='text-[#344054] opacity-[0.5] text-[16px] font-bold text-center '>Registration Officer</div>
+            <div className='my-[64px] text-[#344054] '>
+           <Link href='/regofficer'> <div className='flex hover:text-[#00B3D1]'>
               <Image src={dash} alt='dashboard' className='' />
               <div className='text-[16px] font-semibold ml-[8px] '>Overview</div>
-            </div>
+            </div></Link>
+            <Link href='/hod/student'>
             <div className='flex mt-[24px] hover:text-[#00B3D1] '>
-              <Image src={students} alt='dashboard' className='' />
+              <Image src={students} alt='student' className='' />
               <div className='text-[16px] font-semibold ml-[8px] '>Students</div>
             </div>
-            <Link href='/student/course'>
+            </Link>
+            
+            <Link href='/regofficer/regpage'>
               <div className='flex mt-[24px] hover:text-[#00B3D1] '>
-                <Image src={course} alt='dashboard' className='' />
-                <div className='text-[16px] font-semibold ml-[8px] '>Registration</div>
+                <Image src={course} alt='register' className='' />
+               <div className='text-[16px] font-semibold ml-[8px] '>Registration</div>
               </div>
             </Link>
             
             <div className='flex mt-[24px] hover:text-[#00B3D1] '>
-              <Image src={notification} alt='dashboard' className='' />
+              <Image src={notification} alt='notify' className='' />
               <div className='text-[16px] font-semibold ml-[8px] '>Notification</div>
             </div>
             <div className='flex mt-[24px] hover:text-[#00B3D1] '>
-              <Image src={settings} alt='dashboard' className='' />
+              <Image src={settings} alt='setting' className='' />
               <div className='text-[16px] font-semibold ml-[8px] '>Settings</div>
             </div>
             <div className='flex mt-[24px] hover:text-[#00B3D1] '>
-              <Image src={resources} alt='dashboard' className='' />
+              <Image src={resources} alt='resource' className='' />
               <div className='text-[16px] font-semibold ml-[8px] '>Resources</div>
             </div>
             <div className='flex mt-[24px] hover:text-[#00B3D1] '>
-              <Image src={help} alt='dashboard' className='' />
+              <Image src={help} alt='help' className='' />
               <div className='text-[16px] font-semibold ml-[8px] '>Help and Support</div>
             </div>
           </div>
@@ -58,9 +62,9 @@ const AdminNav = () => {
             </div>
         </div>
 
-      </div>
+    </div>
     </>
   )
 }
 
-export default AdminNav
+export default RegNav
